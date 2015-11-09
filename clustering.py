cdb8d37beb7037c715488ffcef9b2da4101770b4
Han Wang <wanghan15@gmail.com>
2015-11-08 21:03:51 -0500

    Get vector representations of relations with word2vec.

--- clustering.py
+++ clustering.py
@@ -2,12 +2,14 @@
 
 import MySQLdb
 import gensim
+import numpy as np
 
+from relation import Relation
 from ConfigParser import SafeConfigParser
 
 
 def select_sql(table_name='svo', limit=10):
-    return 'SELECT subject, predicate, object FROM {} LIMIT {}'.format(table_name, limit)
+    return 'SELECT subject, predicate, object FROM {} ORDER BY id LIMIT {}'.format(table_name, limit)
 
 
 parser = SafeConfigParser()
@@ -23,15 +25,39 @@ mysql_config = {
 db = MySQLdb.connect(**mysql_config)
 cur = db.cursor()
 try:
-    cur.execute(select_sql())
+    db_result_size = 12479
+    cur.execute(select_sql(limit=db_result_size))
 except MySQLdb.Error, e:
     try:
         print "MySQL Error [{}]: {}".format(e.args[0], e.args[1])
     except IndexError:
         print "MySQL Error: {}".format(str(e))
 else:
+    model_dim = 200
+    model = gensim.models.Word2Vec.load_word2vec_format('wikipedia-pubmed-and-PMC-w2v.bin', binary=True)
+    relation_vectors = np.zeros((db_result_size, model_dim))
+    count = 0
+    oov_count = 0
+    word_count = 0
     for row in cur.fetchall():
-        print row
+        # print row
+        subj, pred, obj = row
+        vec = np.zeros(model_dim)
+        word_list = subj.split() + obj.split()
+        for word in word_list:
+            word = word.strip()
+            if word:
+                word_count += 1
+                if word in model:
+                    vec += model[word]
+                else:
+                    print u'[OOV]: {}'.format(word)
+                    oov_count += 1
+        relation_vectors[count] = vec
+        count += 1
+    np.savetxt('vectors.vec', relation_vectors)
+    print 'Out of vocabulary count: {}'.format(str(oov_count))
+    print 'Total word count: {}'.format(str(word_count))
 finally:
     cur.close()
     db.close()
--- relation.py
+++ relation.py
@@ -3,17 +3,10 @@
 
 class Relation(object):
 
-    def __init__(self, subj=None, pred=None, obj=None, vec_repr=False):
+    def __init__(self, subj=None, pred=None, obj=None):
         self._subj = subj
         self._pred = pred
         self._obj = obj
-        if vec_repr:
-            self._subj_vec_list = []
-            self._pred_vec_list = []
-            self._obj_vec_list = []
-            self._subj_vec = None
-            self._pred_vec = None
-            self._obj_vec = None
 
     def __str__(self):
         return u'({}, {}, {})'.format(str(self._subj), str(self._pred), str(self._obj))
@@ -44,42 +37,3 @@ class Relation(object):
     @object.setter
     def object(self, obj):
         self._obj = obj
-
-    @property
-    def subject_vector(self):
-        return self._subj_vec
-
-    @property
-    def predicate_vector(self):
-        return self._pred_vec
-
-    @property
-    def object_vector(self):
-        return self._obj_vec
-
-    def append_vector(self, component, vector):
-        if component == 'subject':
-            self._subj_vec_list.append(vector)
-        elif component == 'predicate':
-            self._pred_vec_list.append(vector)
-        elif component == 'object':
-            self._obj_vec_list.append(vector)
-        else:
-            raise ValueError('There is no such a variable: {}.'.format(component))
-
-    def compose_vectors(self, component):
-        if component == 'subject':
-            for vec in self._subj_vec_list:
-                self._subj_vec += vec
-        elif component == 'predicate':
-            for vec in self._pred_vec_list:
-                self._pred_vec += vec
-        elif component == 'object':
-            for vec in self._obj_vec_list:
-                self._obj_vec += vec
-        else:
-            raise ValueError('There is no such a variable: {}.'.format(component))
-
-    @staticmethod
-    def save_vector_to_file(vec, file):
-        pass
