843b127a58d8e3dbf07f1eb934285c542adfb5b8
Han Wang <wanghan15@gmail.com>
2015-11-10 14:45:00 -0500

    Refactoring.

--- clustering.py
+++ clustering.py
@@ -4,8 +4,8 @@ import numpy as np
 from pyxmeans import XMeans as xmeans
 
 
-dataset = 'genes-cancer'
-# dataset = 'RiMG75'
+# dataset = 'genes-cancer'
+dataset = 'RiMG75'
 
 embedding_file = 'data/{}/subj_obj_embeddings.txt'.format(dataset)
 data = np.loadtxt(embedding_file)
