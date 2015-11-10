5f07ad50e1e9b8ee5c1455c2edd3fff97a5c693f
Han Wang <wanghan15@gmail.com>
2015-11-09 19:43:39 -0500

    Add xmeans clustering.

---
+++ clustering.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+
+import numpy as np
+from pyxmeans import XMeans as xmeans
+
+
+dataset = 'genes-cancer'
+# dataset = 'RiMG75'
+
+embedding_file = 'data/{}/subj_obj_embeddings.txt'.format(dataset)
+data = np.loadtxt(embedding_file)
+x_means = xmeans(2)
+x_means.fit(data)
