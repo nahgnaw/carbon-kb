a144498227aa6633033eae751e48cd4b72bb13e8
Han Wang <wanghan15@gmail.com>
2015-12-14 21:23:18 -0500

    Construct simple graphs where entities and relations are both nodes.

---
+++ construct_graph.py
@@ -0,0 +1,73 @@
+# -*- coding: utf8 -*-
+
+import codecs
+import yaml
+import logging
+import logging.config
+import MySQLdb
+
+from ConfigParser import SafeConfigParser
+from collections import Counter
+
+
+def read_triples_from_db(sql_query, db):
+    parser = SafeConfigParser()
+    parser.read('config/mysql_config.ini')
+    mysql_config = {
+        'host': parser.get('MySQL', 'host'),
+        'user': parser.get('MySQL', 'user'),
+        'passwd': parser.get('MySQL', 'passwd'),
+        'db': db
+    }
+    conn = MySQLdb.connect(**mysql_config)
+    cur = conn.cursor()
+
+    cur.execute(sql_query)
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return results
+
+
+def save_graph_to_file(graph, output_file, logger):
+    f = codecs.open(output_file, 'w', 'utf-8')
+    for vertex in graph:
+        counts = Counter(graph[vertex])
+        for edge in counts:
+            weight = str(counts[edge])
+            f.write(u'{}\t{}\t{}\n'.format(vertex, edge, weight))
+            f.write(u'{}\t{}\t{}\n'.format(edge, vertex, weight))
+            logger.debug(u'{}\t{}\t{}'.format(vertex, edge, weight))
+    f.close()
+
+
+# Every subject_head, predicate, and object_head is considered as a vertex.
+# An edge connect a pair of {subject_head, predicate} or {object_head, predicate}.
+# The edge weight is the count of the cooccurence of the two connected vertexes.
+def construct_simple_graph(mysql_db, logger):
+    sql_query = u"""
+        SELECT subject_head, predicate, object_head
+        FROM svo
+    """
+    triples = read_triples_from_db(sql_query, mysql_db)
+    graph = {}
+    for triple in triples:
+        subject_head, predicate, object_head = triple
+        subject_head = subject_head.strip().replace(' ', '_')
+        predicate = predicate.strip().replace(' ', '_')
+        object_head = object_head.strip().replace(' ', '_')
+        graph.setdefault(subject_head, []).append(predicate)
+        graph.setdefault(object_head, []).append(predicate)
+    return graph
+
+
+if __name__ == '__main__':
+    with open('config/logging_config.yaml') as f:
+        logging.config.dictConfig(yaml.load(f))
+    logger = logging.getLogger('construct_graph')
+
+    dataset = 'genes-cancer'
+    simple_graph_file = 'data/{}/simple_graph.txt'.format(dataset)
+    mysql_db = 'bio-kb'
+    graph = construct_simple_graph(mysql_db, logger)
+    save_graph_to_file(graph, simple_graph_file, logger)
