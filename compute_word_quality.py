f61f49721b6a88a9eb07c34ed16a36bcb4449874
Han Wang <wanghan15@gmail.com>
2016-05-14 22:59:30 -0400

    Update logging.

--- compute_word_quality.py
+++ compute_word_quality.py
@@ -17,7 +17,7 @@ from textblob import TextBlob
 
 class WordQuality(object):
 
-    def __init__(self, logger, stopwords=None):
+    def __init__(self, logger=None, stopwords=None):
         self.logger = logger if logger else logging.getLogger()
         self._stopwords = set(nltk.corpus.stopwords.words('english')) if stopwords is None else stopwords
 
@@ -110,6 +110,8 @@ def generate_ignored_word_dict():
 
 
 if __name__ == '__main__':
+    with open('config/logging_config.yaml') as f:
+        logging.config.dictConfig(yaml.load(f))
 
     # generate_idf_dict()
     generate_ignored_word_dict()
