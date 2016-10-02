7760d9923f517f42386a3a90fc422d7f6ab07e2d
Han Wang <wanghan15@gmail.com>
2016-10-02 15:51:20 -0400

    Refactor kb graph construction.

--- construct_kb_graph.py
+++ construct_kb_graph.py
@@ -30,14 +30,15 @@ def read_triples_from_db(sql_query, db):
     return results
 
 
-def save_graph_to_file(graph, output_file, logger):
+def save_graph_to_file(graph, output_file, min_edge_count, logger):
     f = codecs.open(output_file, 'w', 'utf-8')
     for vertex in graph:
         counts = Counter(graph[vertex])
         for neighbor in counts:
-            weight = str(counts[neighbor])
-            f.write(u'{}\t{}\t{}\n'.format(vertex, neighbor, weight))
-            logger.debug(u'{}\t{}\t{}'.format(vertex, neighbor, weight))
+            weight = counts[neighbor]
+            if weight > int(min_edge_count):
+                f.write(u'{}\t{}\t{}\n'.format(vertex, neighbor, str(weight)))
+                logger.debug(u'{}\t{}\t{}'.format(vertex, neighbor, str(weight)))
     f.close()
 
 
@@ -52,7 +53,7 @@ def build_directed_graph_from_db(mysql_db, logger):
     triples = read_triples_from_db(sql_query, mysql_db)
     graph = {}
     for triple in triples:
-        logger.debug(triple)
+        # logger.debug(triple)
         subject_head, predicate_canonical, object_head = triple
         subject_head = subject_head.strip().replace(' ', '_')
         predicate_canonical = predicate_canonical.strip().replace(' ', '_')
@@ -63,16 +64,16 @@ def build_directed_graph_from_db(mysql_db, logger):
 
 
 @begin.subcommand
-def construct_graph(dataset, mysql_db):
+def construct_graph(output_file, dataset, mysql_db, min_edge_count):
     with open('config/logging_config.yaml') as f:
         logging.config.dictConfig(yaml.load(f))
     logger = logging.getLogger('construct_kb_graph')
 
-    graph_file = 'data/{}/kb_directed_graph.txt'.format(dataset)
+    graph_file = 'data/{}/embeddings/{}'.format(dataset, output_file)
     logger.info('Start reading triples from db ...')
     graph = build_directed_graph_from_db(mysql_db, logger)
     logger.info('Start constructing kb graph ...')
-    save_graph_to_file(graph, graph_file, logger)
+    save_graph_to_file(graph, graph_file, min_edge_count, logger)
 
 
 @begin.start
