fe7ed7ab39eb4d50a1c12344a3d1ec14a400514e
Han Wang <wanghan15@gmail.com>
2016-10-02 23:30:36 -0400

    Refactor word2vec training.

--- train_word2vec.py
+++ train_word2vec.py
@@ -5,6 +5,7 @@ import codecs
 import logging
 import logging.config
 import yaml
+import begin
 
 from gensim.models import Word2Vec, Phrases
 
@@ -26,14 +27,22 @@ class MySentences(object):
                                 yield line.split()
 
 
-with open('config/logging_config.yaml') as f:
-    logging.config.dictConfig(yaml.load(f))
-logger = logging.getLogger()
+@begin.subcommand
+def train(corpus_path, model_path):
+    with open('config/logging_config.yaml') as f:
+        logging.config.dictConfig(yaml.load(f))
+    logger = logging.getLogger()
 
-corpus_path = 'data/pmc_c-h/processed_done'
-model_path = 'data/pmc_c-h/embeddings/word2vec'
-sentences = MySentences(corpus_path)
-# bigrams = Phrases(sentences, min_count=1)
-# trigrams = Phrases(bigrams[sentences], min_count=1)
-model = Word2Vec(sentences, size=200, workers=4, sg=1, hs=0, negative=5, min_count=1)
-model.save_word2vec_format(model_path, binary=True)
+    sentences = MySentences(corpus_path)
+    # bigrams = Phrases(sentences, min_count=1)
+    # trigrams = Phrases(bigrams[sentences], min_count=1)
+    model = Word2Vec(sentences, size=200, workers=16, sg=1, hs=0, negative=5, min_count=5)
+    model.save_word2vec_format(model_path, binary=True)
+
+
+@begin.start
+def main():
+    pass
+
+if begin.start():
+    pass
